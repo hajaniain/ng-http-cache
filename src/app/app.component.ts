c7c4e8e9c581121a15ef27c0f2cc0c169f9b43fa
hajaniain <hajaniainamarius@gmail.com>
2019-11-19 10:42:57 +0100

    feat(example): improve example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CacheService } from './services/cache.service';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,12 @@ import { map } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'ng-http-cache';
+  countries: Observable<any>;
+  countrySelected: any;
   constructor(public http: HttpClient, public cache: CacheService) {
-    //Cache an observable
-    let requestObservable = this.http.get('https://restcountries.eu/rest/v2/all').pipe(map(res => res));
+    // Cache an observable
+    const requestObservable = this.http.get('https://restcountries.eu/rest/v2/all').pipe(map(res => res));
 
-    this.cache.observable('my-cache-key', requestObservable, 1).subscribe(result => {
-      //Use result
-      console.log(result);
-    });
+    this.countries = this.cache.observable('my-cache-key', requestObservable, 1);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, MatSelectModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
